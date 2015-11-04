1cfffee01af9f172ab109d4fa2e0c08ac2b57886
Christopher Leabeter <christopherleabeter@gmail.com>
2015-11-04 14:23:07 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budgie::Application.config.secret_key_base = '7d7d7908ab9c14a25575723ceb1fd9ed71527ae87da2340309e6150e7e70655f95236a0b07ff1ddf4dd1477ad4a09f2e52a898683d00a3bbf4006d8c4c6837ed'
