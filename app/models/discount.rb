9d1d46e585dc80d9f628181e1f52a33a87454e31
vagrant <natsanjuro@gmail.com>
2015-11-06 19:31:30 +0000

    companies with discounts do not show up

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -8,7 +8,12 @@ class Discount < ActiveRecord::Base
               numericality: { only_integer: true},
               inclusion: { in: 1..100, message: "must be between 1 and 100"}
 
+  validates :restrictions,
+              presence: true,
+              length: { in: 5..50 }
+                
   scope :with_user, -> { includes(:user)
     .where.not( users:{id: nil}) }            
+
 
 end
