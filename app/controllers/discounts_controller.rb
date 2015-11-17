c56314c96d1f54c73bf9ba5ba2d0d533da3c15c5
vagrant <natsanjuro@gmail.com>
2015-11-17 21:14:57 +0000

    added ratings

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,6 +1,6 @@
 class DiscountsController < ApplicationController
   before_action :set_discount, only: [:show, :edit, :update, :destroy]
-  # before_action :set_user
+  # before_action :set_user 
   before_action :set_company
 
  
