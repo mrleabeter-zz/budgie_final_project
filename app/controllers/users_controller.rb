2e05123b8e46298c17a0be72ef58d519f732fbdb
Wilczanora <ralph.brzozowski@gmail.com>
2015-11-18 04:38:36 +0000

    Fixed card descriptions/fonts/icons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  #before_action :set_company_and_discount
+  before_action :set_company_and_discount
 
   # GET /users
   # GET /users.json
