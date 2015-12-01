4295a2c9cd277883e63e726ddee7493161e1485c
Wilczanora <ralph.brzozowski@gmail.com>
2015-12-01 21:23:49 +0000

    final changes to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,17 @@
 
 require 'faker'
 
-50.times do 
+5.times do 
 password = Faker::Internet.password(8)
 User.create(:username => Faker::Internet.user_name, :email => Faker::Internet.free_email, :password => password, :password_confirmation => password, :city => "Toronto") 
 
 end
 
-30.times do Company.create(:company_name => Faker::Company.name) 
+5.times do Company.create(:company_name => Faker::Company.name) 
 
 end
 
-50.times do Discount.create(:discount_percent => Faker::Number.between(5, 40), 
-  :user_id => Faker::Number.between(1, 50), :company_id => Faker::Number.between(1, 30), :restrictions => Faker::Lorem.sentence) 
+10.times do Discount.create(:discount_percent => Faker::Number.between(5, 40), 
+  :user_id => Faker::Number.between(1, 5), :company_id => Faker::Number.between(1, 5), :restrictions => Faker::Lorem.sentence) 
 
 end
